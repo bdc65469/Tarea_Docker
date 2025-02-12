1866cd23679e1375bd98eaa42be168fad395e514
bdc65469 <bdc65469@educastur.es>
2025-02-12 13:14:43 +0100

    Carpeta organizada
    Organizados todos los archivos en una carpeta

---
+++ Ejercicio2/Script Ejercicio2/tablas.sql
@@ -0,0 +1,20 @@
+USE prueba;
+
+-- Crear la tabla personas
+CREATE TABLE IF NOT EXISTS personas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    edad INT CHECK (edad >= 0),
+    email VARCHAR(100) UNIQUE
+);
+
+-- Crear la tabla coches
+CREATE TABLE IF NOT EXISTS coches (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR(50) NOT NULL,
+    modelo VARCHAR(50) NOT NULL,
+    año INT CHECK (año >= 1886), -- Año del primer automóvil
+    propietario_id INT,
+    FOREIGN KEY (propietario_id) REFERENCES personas(id) ON DELETE SET NULL
+);
